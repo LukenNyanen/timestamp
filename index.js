0cd37035d231b2bb110698f05638f0b7e63c9f88
Lasse Jørgensen <28780271+lasjorg@users.noreply.github.com>
2024-06-03 22:35:08 +0200

    fix(config): remove theme config (#111)

--- index.js
+++ index.js
@@ -24,6 +24,32 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+const isInvalideDate = (date)=>date.toUTCString()==="Invalid Date"
+
+app.get("/api/:date", (req,res)=>{
+  let date = new Date(req.params.date)  
+  
+  if(isInvalideDate(date)){
+  date = new Date(+req.params.date)
+  }
+  if(isInvalideDate(date)){
+  res.json({error:"Invalid Date"})
+  return
+  }
+  res.json({
+  unix: date.getTime(),
+  utc:date.toUTCString()
+  })
+  })
+  
+  app.get("/api", (req,res)=>{
+  res.json({
+  unix: new Date().getTime(),
+  utc: new Date().toUTCString()
+  })
+  })
+  
+
 
 
 // Listen on port set in environment variable or default to 3000
